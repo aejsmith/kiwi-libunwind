cd2d982f6e9a02556f35ee2cd1d1df7a43dc46f5
Alex Smith <alex@alex-smith.me.uk>
2013-08-18 11:34:04 +0100

    Fixed x86_64 compilation on non-Linux OSes.

--- include/tdep-x86_64/libunwind_i.h
+++ include/tdep-x86_64/libunwind_i.h
@@ -192,12 +192,13 @@ dwarf_put (struct dwarf_cursor *c, dwarf_loc_t loc, unw_word_t val)
 # define tdep_fetch_frame		UNW_OBJ(fetch_frame)
 # define tdep_cache_frame		UNW_OBJ(cache_frame)
 # define tdep_reuse_frame		UNW_OBJ(reuse_frame)
+# define tdep_stash_frame		UNW_OBJ(stash_frame)
 #else
 # define tdep_fetch_frame(c,ip,n)	do {} while(0)
 # define tdep_cache_frame(c,rs)		do {} while(0)
 # define tdep_reuse_frame(c,rs)		do {} while(0)
+# define tdep_stash_frame(c,rs)		do {} while(0)
 #endif
-#define tdep_stash_frame		UNW_OBJ(stash_frame)
 #define tdep_trace			UNW_OBJ(tdep_trace)
 #define x86_64_r_uc_addr                UNW_OBJ(r_uc_addr)
 
